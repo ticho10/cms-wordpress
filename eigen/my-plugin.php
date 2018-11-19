2db7f9f30c9ecb115e074422d8077aca43dfc837
ticho10 <poffertjeskip@gmail.com>
2018-11-19 22:08:22 +0100

    plugin

---
+++ eigen/my-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Mijn eigen plugin ticho plug
+Plugin URI:   http://25030.hosts1.ma-cloud.nl/bewijzenmap/periode2.1/cms/opdracht3/wordpress/wp-content/plugins/eigen
+Description:  Simpele eigen plugin voor CMS
+Version:      0.1
+Author:		  Ticho Plug
+Author URI:   ttp://25030.hosts1.ma-cloud.nl/bewijzenmap/periode2.1/cms/opdracht3/wordpress/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
