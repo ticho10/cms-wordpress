ea4935fa052a030e5fc444d75abf063adc01c41f
ticho10 <poffertjeskip@gmail.com>
2018-11-27 22:49:58 +0100

    Update my-plugin.php

--- eigen/my-plugin.php
+++ eigen/my-plugin.php
@@ -8,14 +8,24 @@ Author:		  Ticho Plug
 Author URI:   ttp://25030.hosts1.ma-cloud.nl/bewijzenmap/periode2.1/cms/opdracht3/wordpress/
 License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:  wporg
+Text Domain:  ticho-plug-plugin
 Domain Path:  /languages
 */
 
 if( !defined( 'ABSPATH' ) ) exit;
 
 
-	 $args = array(
+	
+function test_register_post_type() {
+	
+
+	
+    $labels = array( 'name' => _x( 'Portfolio', 'Post type general name', 'ticho-plug-plugin' ), 'singular_name' => _x( 'Portfolio Item', 'Post type singular name', 'ticho-plug-plugin' ),
+        'menu_name' => _x( 'Portfolio Items', 'Admin Menu text', 'ticho-plug-plugin' ),
+        'name_admin_bar' => _x( 'Portfolio Items', 'Add New on Toolbar', 'ticho-plug-plugin' ),
+);
+
+ $args = array(
 	    'labels'             => $labels,
 	    'public'             => true,
 	    'publicly_queryable' => true,
@@ -30,15 +40,6 @@ if( !defined( 'ABSPATH' ) ) exit;
 	    'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
 	    'menu_icon'			 => 'dashicons-screenoptions',
     );
-function test_register_post_type() {
-	
-
-	
-    $labels = array( 'name' => _x( 'Portfolio', 'Post type general name', 'Mijn eigen plugin ticho plug' ), 'singular_name' => _x( 'Portfolio Item', 'Post type singular name', 'test-portfolio' ),
-        'menu_name' => _x( 'Portfolio Items', 'Admin Menu text', 'Mijn eigen plugin ticho plug' ),
-        'name_admin_bar' => _x( 'Portfolio Items', 'Add New on Toolbar', 'Mijn eigen plugin ticho plug' ),
-);
-
 
 
 register_post_type( 'test_portfolio', $args );
